1141a2f71de876cb5a2289d77b0e0a84c07f3670
Monika1121 <sherkhanemonika2@gmail.com>
2026-02-06 14:17:27 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Headerr from "./assets/header";
+
+function App() {
+  return (
+   <>
+  <div className="w-100 h-50 border border-primary ">
+    ESTATE FLOW
+  </div>
+  <Headerr/>
+  </>
+   
+  );
+}
+
+export default App;
---
+++ src/assets/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ const Headerr = () => {
+  return (
+    <div>header</div>
+  )
+}
+
+export default Headerr
